3f27c8cd0168b228e5ccc13ca9448524b06ebb6a
Norumai01 <johnnydinhnguyen2001@gmail.com>
2024-10-16 05:28:11 -0400

    Addded click-able menu on profile pic

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React from "react";
 import AuthPage from "./components/Auth/AuthPage";
 import Dashboard from "./components/Social_Hub/Dashboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      // Empty object will prevent Chakra from applying its global styles
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<AuthPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </Router>
+    <ChakraProvider theme={theme} resetCSS={false}>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<AuthPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </ChakraProvider>
   );
 }
 
